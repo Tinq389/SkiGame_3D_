5ebb8c2f3478ae8fb792e981188a071b2156065e
i <149830911+Tinq389@users.noreply.github.com>
2025-04-10 13:19:03 +0300

    Snowmen throw optimization things

--- SkiGame_3D_/Assets/Scripts/MoveMover.cs
+++ SkiGame_3D_/Assets/Scripts/MoveMover.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class MoveMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /* Update is called once per frame
+    void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    for(int i = 0; i < 150000; i++)
-    //    {
-    //        GameObject.Find("Player");
-    //    }
-    //}
+        for(int i = 0; i < 150000; i++)
+        {
+            GameObject.Find("Player");
+        }
+    }*/
 }
---
+++ SkiGame_3D_/Assets/Scripts/Snowball.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
